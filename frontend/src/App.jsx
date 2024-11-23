c9c905d9d80117077bdf079de0f20b245c08fdd6
Anderson Amicuchi <andersonamicuchi@gmail.com>
2024-11-23 16:13:15 -0300

    Instalação do React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -5,11 +6,11 @@ import './styles/globals.css';
 
 function App() {
   return (
-    <div className="font-sans bg-[#f8fafc] text-gray-800 min-h-screen">
+    <Router className="font-sans bg-[#f8fafc] text-gray-800 min-h-screen">
       <Header />
       <MainContent />
       <Footer />
-    </div>
+    </Router>
   );
 };
 
