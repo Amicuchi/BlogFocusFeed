b2eb5771bc73014cb2c8dc2a7a04370dc897bef8
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-09 21:14:12 -0300

    feat(App.jsx): Utilização do contexto de autenticação no entorno das rotas.
    Com a inclusão da autenticação em forma de contexto geral, essa funcionalidade passa a ficar disponível em qualquer lugar da aplicação.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { AuthProvider } from './contexts/AuthProvider';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <AppRoutes />
-      <Footer />
+      <AuthProvider>
+        <Header />
+        <AppRoutes />
+        <Footer />
+      </AuthProvider>
     </Router>
   )
 }
