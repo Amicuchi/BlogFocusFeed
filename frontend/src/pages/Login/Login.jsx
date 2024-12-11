991ec67b9ead1497b590356bf05654704385f26f
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-11 20:27:50 -0300

    fix(Login): Correção de endereço de importação de componente

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthProvider.jsx';
+import { useAuth } from '../../contexts/AuthContext.jsx';
 import apiServices from '../../services/apiServices';
 import styles from './Login.module.css';
 
