79474978bef14040a0f553be74d43566f1c5271a
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-12 21:04:53 -0300

    refactor(AuthContext): mudança de nome de variável
    - Alteração no nome da variável de usuário logado (isAuthenticated para signed)
    - Melhoria da legibilidade via exclusão de comentários desnecessários
    - Melhoria da manutenibilidade via alteração de nomes de variáveis (jwt_token para user_token_

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,48 +1,34 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-// Componente de contexto para autenticação
 export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [user, setUser] = useState(null);
 
-    // Carrega o estado inicial ao montar o componentea partir do localStorage
     useEffect(() => {
-        // Verifica se o token existe no localStorage ao montar o componente
-        const token = localStorage.getItem('jwt_token');
+        const userToken = localStorage.getItem('user_token');
         const userData = localStorage.getItem('user_data');
 
-        if (token && userData) {
-            try {
-                setUser(JSON.parse(userData));
-                setIsAuthenticated(true);
-            } catch (error) {
-                console.error('Erro ao parsear user_data:', error);
-                localStorage.removeItem('user_data'); // Remove dado inválido
-            }
+        if (userToken && userData) {
+            setUser(JSON.parse(userData));
         }
     }, []);
 
-    // Função para realizar login
-    const login = (token, userData) => {
-        localStorage.setItem('jwt_token', token); // Armazena o token
-        localStorage.setItem('user_data', JSON.stringify(userData));
-        setIsAuthenticated(true);             // Atualiza o estado para autenticado
-        setUser(userData);
+    const login = (token, user) => {
+        localStorage.setItem('user_token', token);
+        localStorage.setItem('user_data', JSON.stringify(user));
+        setUser(user);
     };
 
-    // Função para realizar o logout e remover o token do localStorage
     const logout = () => {
-        localStorage.removeItem('jwt_token');   // Remove o token
-        localStorage.removeItem('user_data');   // Remove os dados do usuário do localStorage
-        setIsAuthenticated(false);              // Atualiza o estado para não autenticado
-        setUser(null);                          // Remove os dados do usuário do estado
+        localStorage.removeItem('user_token');
+        localStorage.removeItem('user_data');
+        setUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+        <AuthContext.Provider value={{ signed: !!user, user, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
@@ -52,5 +38,4 @@ AuthProvider.propTypes = {
     children: PropTypes.node.isRequired,
 };
 
-// Hook para acessar o contexto de autenticação
 export const useAuth = () => useContext(AuthContext);
