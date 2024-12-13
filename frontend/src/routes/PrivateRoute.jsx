cded23c5951ab7823c88cb8444b03d97c312a89a
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-13 20:45:58 -0300

    style(PrivateRoute): Mera exclusão de linhas vazias para melhoria visual.

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -2,19 +2,16 @@ import PropTypes from 'prop-types';
 import Login from '../pages/Login/Login';
 
 function PrivateRoute({ children }) {
-
   const token = localStorage.getItem('token');
 
   if (!token) {
     console.warn('Token não encontrado.');
     return <Login />;
   }
-
   return children;
 }
 
 PrivateRoute.propTypes = {
   children: PropTypes.node,
 };
-
 export default PrivateRoute;
