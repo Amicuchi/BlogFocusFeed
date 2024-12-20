66dfdfbc4e0337d53405c8329abe1ac173c884e3
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-20 19:47:35 -0300

    feat(Server): Inclusão da rota de autenticação.

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import userRoutes from './src/routes/userRoutes.js';
 import postRoutes from './src/routes/postRoutes.js';
 import categoryRoutes from './src/routes/categoryRoutes.js';
 import contactRoutes from './src/routes/contactRoutes.js';
+import forgotPasswordRoutes from './routes/forgotPasswordRoutes.js';
 
 dotenv.config();
 
@@ -44,6 +45,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/contactadmin', contactRoutes);
+app.use('/api/auth', forgotPasswordRoutes);
 
 // Handlers de erro
 app.use(notFoundHandler);
