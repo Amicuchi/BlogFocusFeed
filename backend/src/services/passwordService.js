258b8d22c340be342e4745eb0bd4b1cf3441fc50
Anderson Amicuchi <AndersonAmicuchi@gmail.com>
2024-12-21 18:50:35 -0300

    style(passwordService): Alterações menores no código e nas aspas e espaços. Nada significativo.

--- backend/src/services/passwordService.js
+++ backend/src/services/passwordService.js
@@ -9,11 +9,11 @@ export const generateResetToken = async (email) => {
     throw new Error("Usuário não encontrado");
   }
 
-  const resetToken = crypto.randomBytes(32).toString('hex');
+  const resetToken = crypto.randomBytes(32).toString("hex");
   const hashedToken = crypto
-    .createHash('sha256')
+    .createHash("sha256")
     .update(resetToken)
-    .digest('hex');
+    .digest("hex");
 
   user.passwordResetToken = hashedToken;
   user.passwordResetExpires = Date.now() + 3600000; // 1 hora
@@ -25,23 +25,22 @@ export const generateResetToken = async (email) => {
 };
 
 export const updatePassword = async (token, newPassword) => {
-    const hashedToken = crypto
-    .createHash('sha256')
-    .update(token)
-    .digest('hex');
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
 
   const user = await User.findOne({
     passwordResetToken: hashedToken,
-    passwordResetExpires: { $gt: Date.now() }
+    passwordResetExpires: { $gt: Date.now() },
   });
 
   if (!user) {
+    console.error("Token inválido ou expirado.");
     throw new Error("Token inválido ou expirado");
   }
-
+  
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(newPassword, salt);
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
+
   await user.save();
 };
