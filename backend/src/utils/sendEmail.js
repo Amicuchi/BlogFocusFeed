b7e24b904bf5993d06f17886f9ca1d76da453c23
Anderson Amicuchi <AndersonAmicuchi@gmail.com>
2024-12-21 17:01:41 -0300

    feat(SendEmail): Configuração do componente responsável por envio de emails de recuperação de senha

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,10 +1,12 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 // Função para enviar e-mails
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // ou outro provedor (verifique o suporte do seu provedor)
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER, // Email do remetente
         pass: process.env.EMAIL_PASS, // Senha ou App Password do email
@@ -19,10 +21,10 @@ const sendEmail = async (to, subject, text) => {
     };
 
     await transporter.sendMail(mailOptions);
-    console.log('E-mail enviado com sucesso para:', to);
+    console.log("E-mail enviado com sucesso para:", to);
   } catch (error) {
-    console.error('Erro ao enviar e-mail:', error);
-    throw new Error('Erro ao enviar e-mail');
+    console.error("Erro ao enviar e-mail:", error);
+    throw new Error("Erro ao enviar e-mail");
   }
 };
 
