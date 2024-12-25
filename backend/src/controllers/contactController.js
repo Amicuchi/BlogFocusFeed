6fff719285207779b387dd6c07bdb0d2e062bc45
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-12-25 14:43:22 -0300

    fix(contact): Atualização do controller de contato para que funcione adequadamente.
    - Foi alterado o serviço de nodemailer para sendMail.
    - A partir da alteração anterior, foi alterado o funcionamento do restante do componente.
    
    Está funcional.

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -1,31 +1,29 @@
-import nodemailer from 'nodemailer';
+import sendEmail from "../utils/sendEmail.js";
 
-export const contactAdmin = async (req, res) => {
-  const { name, email, message } = req.body;
+export const contactAdmin = async (req, res, next) => {
+    const { name, email, message } = req.body;
 
-  if (!name || !email || !message) {
-    return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
-  }
+    if (!name || !email || !message) {
+        return res.status(400).json({
+            error: "Todos os campos são obrigatórios.",
+        });
+    }
 
-  try {
-    // Configuração do transporte de email
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: process.env.ADMIN_EMAIL,
-        pass: process.env.ADMIN_EMAIL_PASSWORD,
-      },
-    });
+    try {
+        await sendEmail({
+            to: process.env.EMAIL_USER,
+            subject: `Mensagem de ${name}`,
+            html: `
+        <h1>Nova Mensagem de Contato</h1>
+        <p><strong>Nome:</strong> ${name}</p>
+        <p><strong>E-mail:</strong> ${email}</p>
+        <p><strong>Mensagem:</strong></p>
+        <p>${message}</p>
+      `,
+        });
 
-    await transporter.sendMail({
-      from: `"${name}" <${email}>`,
-      to: process.env.ADMIN_EMAIL,
-      subject: `Nova mensagem de ${name}`,
-      text: message,
-    });
-
-    res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao enviar mensagem: ' + error.message });
-  }
+        res.status(200).json({ message: "Mensagem enviada com sucesso!" });
+    } catch (error) {
+        next(error);
+    }
 };
