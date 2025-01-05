f17827bbe7f2b348446f55034d9463d6c78cb200
Anderson Amicuchi <AndersonAmicuchi@gmail.com>
2025-01-04 23:01:22 -0300

    feat(postRoutes): Inclusão do Middleware authorizeRole nas rotas protegidas.

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   createPost,
   getAllPosts,
@@ -8,25 +8,27 @@ import {
   deletePost,
   likePost,
   dislikePost,
-} from '../controllers/postController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
-import validate from '../middlewares/validationMiddleware.js';
-import { postValidationSchema } from '../config/validation.js';
+} from "../controllers/postController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import validate from "../middlewares/validationMiddleware.js";
+import { postValidationSchema } from "../config/validation.js";
+import { UserRoles } from "../models/User.js";
+import authorizeRole from "../middlewares/authorizeRoleMiddleware.js";
 
 const router = express.Router();
 
 // Rotas públicas
-router.get('/', getAllPosts);                           // Todos os posts
-router.get('/search', getAllPosts);                     // Pesquisa (ajustar para usar query params)
-router.get('/:id', getPostById);                        // Detalhes de um post
-router.get('/category/:categoryId', getPostByCategory); // Posts por categoria
+router.get("/", getAllPosts); // Todos os posts
+router.get("/search", getAllPosts); // Pesquisa (ajustar para usar query params)
+router.get("/:id", getPostById); // Detalhes de um post
+router.get("/category/:categoryId", getPostByCategory); // Posts por categoria
 
 // Rotas protegidas
-router.post('/', authMiddleware, validate(postValidationSchema.create), createPost);    // Criar post
-router.put('/:id', authMiddleware, validate(postValidationSchema.create), updatePost);  // Atualizar post
-router.delete('/posts/:id', authMiddleware, deletePost);                                // Deletar post
+router.post("/", authMiddleware, authorizeRole([UserRoles.AUTHOR, UserRoles.MODERATOR, UserRoles.OWNER]), validate(postValidationSchema.create), createPost );
+router.put("/:id", authMiddleware, authorizeRole([UserRoles.AUTHOR, UserRoles.MODERATOR, UserRoles.OWNER]), validate(postValidationSchema.create), updatePost );
+router.delete("/posts/:id", authMiddleware, authorizeRole([UserRoles.AUTHOR, UserRoles.MODERATOR, UserRoles.OWNER]), deletePost );
 
-router.post('/:id/like', authMiddleware, likePost);
-router.post('/:id/dislike', authMiddleware, dislikePost);
+router.post("/:id/like", authMiddleware, likePost);
+router.post("/:id/dislike", authMiddleware, dislikePost);
 
 export default router;
