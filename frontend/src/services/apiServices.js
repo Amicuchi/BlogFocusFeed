842f3991117aa8f1a97c4fe20141b4a391cd96dc
Anderson Amicuchi <AndersonAmicuchi@gmail.com>
2025-01-04 22:55:46 -0300

    feat(apiServices): Inclusão e alteração de serviços.
    Inclusão dos serviços:
    - getUserRole
    - getAllUsers
    - changeUserRole
    - ownerDeleteUser
    
    Alteração do serviço:
    - deleteUser

--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -1,45 +1,48 @@
-import api from './api.js';
+import api from "./api.js";
 
 // Serviço centralizado
 const apiServices = {
-    // Usuário
-    registerUser: (data) => api.post('/api/users/register', data), // Registro de usuário
-    loginUser: (data) => api.post('/api/users/login', data),       // Login de usuário
-    getUserProfile: () => api.get('/api/users/user'),              // Obter perfil do usuário
-    getAuthorProfile: (userId) => api.get(`/api/users/author/${userId}`),      // Obter perfil de autor
-    updateUserProfile: (data) => api.put('/api/users/user', data), // Atualizar perfil do usuário
-    deleteUser: () => api.delete('/api/users/user'),
-    
-    // Contato
-    sendContactMessage: (formData) => api.post('/api/contact/contact', formData),  // Enviar mensagem
+  // Usuário
+  registerUser: (data) => api.post("/api/users/register", data),        // Registro de usuário
+  loginUser: (data) => api.post("/api/users/login", data),              // Login de usuário
+  getUserProfile: () => api.get("/api/users/user"),                     // Obter perfil do usuário
+  getUserRole: () => api.get("/api/users/user/role"),                   // Obter cargo do usuário
+  getAllUsers: () => api.get("/api/users/allusers"),                    // Obter todos os usuários
+  getAuthorProfile: (userId) => api.get(`/api/users/author/${userId}`), // Obter perfil de autor
+  updateUserProfile: (data) => api.put("/api/users/user", data),                                        // Atualizar perfil do usuário
+  changeUserRole: (userId, newRole) => api.put("/api/users/role", { targetUserId: userId, newRole }),   // Alterar cargo do usuário
+  deleteUser: () => api.delete("/api/users/user"),                      // Usuário exclui a própria conta
+  ownerDeleteUser: (userId) => api.delete(`/api/users/${userId}`),      // Proprietário exclui a conta do usuário
 
-    // Autenticação
-    forgotPassword: async (email) => api.post('/api/auth/forgot-password', { email }),                                                              // Solicitar redefinição de senha
-    resetPassword: async (token, currentPassword, newPassword) => api.post('/api/auth/reset-password', { token, currentPassword, newPassword }),    // Redefinição de senha
-    resetPasswordWithToken: async (token, newPassword) => api.post("/api/auth/reset-password-with-token", { token, newPassword }),                  // Redefinição de senha com token
+  // Contato
+  sendContactMessage: (formData) => api.post("/api/contact/contact", formData), // Enviar mensagem
 
-    // Posts
-    getAllPosts: (params) => api.get('/api/posts', { params }),   // Obter todos os posts
-    getPostById: (id) => api.get(`/api/posts/${id}`),             // Obter detalhes de um post
-    getPostsByAuthor: (userId) => api.get(`/api/posts?userId=${userId}`),               // Obter posts de um autor
-    getPostsByCategory: (categoryId) => api.get(`/api/posts/category/${categoryId}`),   // Obter posts por categoria
-    searchPosts: (query) => api.get(`/api/posts/search?query=${query}`),                // Buscar posts por termo de busca
-    createPost: (data) => api.post('/api/posts', data),           // Criar novo post
-    updatePost: (id, data) => api.put(`/api/posts/${id}`, data),  // Atualizar um post
-    deletePost: (id) => api.delete(`/api/posts/posts/${id}`),     // Deletar um post
+  // Autenticação
+  forgotPassword: async (email) => api.post("/api/auth/forgot-password", { email }),                                                            // Solicitar redefinição de senha
+  resetPassword: async (token, currentPassword, newPassword) => api.post("/api/auth/reset-password", { token, currentPassword, newPassword, }), // Redefinição de senha
+  resetPasswordWithToken: async (token, newPassword) => api.post("/api/auth/reset-password-with-token", { token, newPassword }),                // Redefinição de senha com token
 
-    likePost: (id) => api.post(`/api/posts/${id}/like`),        // Curtir um post
-    dislikePost: (id) => api.post(`/api/posts/${id}/dislike`),  // Descurtir um post
+  // Posts
+  createPost: (data) => api.post("/api/posts", data),                               // Criar novo post
+  getPostById: (id) => api.get(`/api/posts/${id}`),                                 // Obter detalhes de um post
+  getPostsByAuthor: (userId) => api.get(`/api/posts?userId=${userId}`),             // Obter posts de um autor
+  getPostsByCategory: (categoryId) => api.get(`/api/posts/category/${categoryId}`), // Obter posts por categoria
+  getAllPosts: (params) => api.get("/api/posts", { params }),                       // Obter todos os posts
+  searchPosts: (query) => api.get(`/api/posts/search?query=${query}`),              // Buscar posts por termo de busca  
+  updatePost: (id, data) => api.put(`/api/posts/${id}`, data),                      // Atualizar um post
+  deletePost: (id) => api.delete(`/api/posts/posts/${id}`),                         // Deletar um post
+  likePost: (id) => api.post(`/api/posts/${id}/like`),                              // Curtir um post
+  dislikePost: (id) => api.post(`/api/posts/${id}/dislike`),                        // Descurtir um post
 
-    // Categorias
-    getAllCategories: () => api.get('/api/categories'),                     // Obter todas as categorias
-    createCategory: (data) => api.post('/api/categories', data),            // Criar nova categoria
-    updateCategory: (id, data) => api.put(`/api/categories/${id}`, data),   // Atualizar categoria
-    deleteCategory: (id) => api.delete(`/api/categories/${id}`),            // Deletar categoria
+  // Categorias
+  getAllCategories: () => api.get("/api/categories"),                   // Obter todas as categorias
+  createCategory: (data) => api.post("/api/categories", data),          // Criar nova categoria
+  updateCategory: (id, data) => api.put(`/api/categories/${id}`, data), // Atualizar categoria
+  deleteCategory: (id) => api.delete(`/api/categories/${id}`),          // Deletar categoria
 
-    // Newsletter
-    newsletterSubscribe: (data) => api.post('/api/newsletter/subscribe', data),         // Inscrever-se na newsletter
-    unsubscribeNewsletter: (token) => api.get(`/api/newsletter/unsubscribe/${token}`),  // Cancelar inscrição na newsletter
+  // Newsletter
+  newsletterSubscribe: (data) => api.post("/api/newsletter/subscribe", data),         // Inscrever-se na newsletter
+  unsubscribeNewsletter: (token) => api.get(`/api/newsletter/unsubscribe/${token}`),  // Cancelar inscrição na newsletter
 };
 
 export default apiServices;
