fa935b2611ceb6ec1ca80f050d0087f5a1037934
Anderson Amicuchi <AndersonAmicuchi@gmail.com>
2025-01-04 22:52:55 -0300

    feat(userRoutes): Inclusão e atualização de rotas.
    Atualização da rota deleteUserProfile para deleteOwnAccount.
    
    Inclusão das rotas:
    - getUserRole
    - getAllUsers
    - changeUserRole
    - deleteUser

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,28 +1,40 @@
-import express from 'express';
+import express from "express";
 import {
   registerUser,
   loginUser,
   getUserProfile,
+  getAuthorProfile,
+  getUserRole,
+  getAllUsers,
   updateUserProfile,
   updateUserEmail,
-  getAuthorProfile,
-  deleteUserProfile
-} from '../controllers/userController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
-import validate from '../middlewares/validationMiddleware.js'
-import { userValidationSchema } from '../config/validation.js';
+  changeUserRole,
+  deleteUser,
+  deleteOwnAccount
+} from "../controllers/userController.js";
+import { userValidationSchema } from "../config/validation.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import validate from "../middlewares/validationMiddleware.js";
+import authorizeRole from "../middlewares/authorizeRoleMiddleware.js";
+import { UserRoles } from "../models/User.js";
 
 const router = express.Router();
 
 // Rotas públicas
-router.post('/register', validate(userValidationSchema.register), registerUser);
-router.post('/login', validate(userValidationSchema.login), loginUser);
-router.get('/author/:id', getAuthorProfile);
+router.post("/register", validate(userValidationSchema.register), registerUser);  // Rota para registrar um usuário
+router.post("/login", validate(userValidationSchema.login), loginUser);           // Rota para logar um usuário
+router.get("/author/:id", getAuthorProfile);                                      // Rota para pegar o perfil de um autor
 
 // Rotas protegidas
-router.get('/user', authMiddleware, getUserProfile);
-router.put('/user', authMiddleware, updateUserProfile);
-router.put('/user/email', authMiddleware, updateUserEmail);
-router.delete('/user', authMiddleware, deleteUserProfile);
+router.get("/user", authMiddleware, getUserProfile);          // Rota para pegar o perfil do usuário logado
+router.get("/user/role", authMiddleware, getUserRole);        // Rota para pegar o cargo do usuário logado
+router.get("/allusers", authMiddleware, authorizeRole([UserRoles.MODERATOR, UserRoles.OWNER]), getAllUsers);  // Rota para pegar todos os usuários
+
+router.put("/user", authMiddleware, updateUserProfile);       // Rota para atualizar o perfil do usuário logado
+router.put("/user/email", authMiddleware, updateUserEmail);   // Rota para atualizar o e-mail do usuário logado
+router.put("/role", authMiddleware, authorizeRole([UserRoles.MODERATOR, UserRoles.OWNER]), changeUserRole);   // Rota para alterar o cargo de um usuário
+
+router.delete("/user", authMiddleware, deleteOwnAccount); // Deletar própria conta
+router.delete("/users/:userId", authMiddleware, authorizeRole([UserRoles.OWNER]), deleteUser); // Owner deleta outro usuário
 
 export default router;
