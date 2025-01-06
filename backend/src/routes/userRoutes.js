d4172ed769140e143fe41e8a2bff57b140e4a017
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2025-01-06 18:03:35 -0300

    chore(userRoutes): Especialização do componente de rotas referentes ao usuário.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,20 +3,16 @@ import {
   registerUser,
   loginUser,
   getUserProfile,
-  getAuthorProfile,
   getUserRole,
-  getAllUsers,
+  getAuthorProfile,
   updateUserProfile,
   updateUserEmail,
-  changeUserRole,
-  deleteUser,
   deleteOwnAccount
 } from "../controllers/userController.js";
 import { userValidationSchema } from "../config/validation.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import validate from "../middlewares/validationMiddleware.js";
-import authorizeRole from "../middlewares/authorizeRoleMiddleware.js";
-import { UserRoles } from "../models/User.js";
+
 
 const router = express.Router();
 
@@ -26,15 +22,11 @@ router.post("/login", validate(userValidationSchema.login), loginUser);         
 router.get("/author/:id", getAuthorProfile);                                      // Rota para pegar o perfil de um autor
 
 // Rotas protegidas
-router.get("/user", authMiddleware, getUserProfile);          // Rota para pegar o perfil do usuário logado
-router.get("/user/role", authMiddleware, getUserRole);        // Rota para pegar o cargo do usuário logado
-router.get("/allusers", authMiddleware, authorizeRole([UserRoles.MODERATOR, UserRoles.OWNER]), getAllUsers);  // Rota para pegar todos os usuários
-
-router.put("/user", authMiddleware, updateUserProfile);       // Rota para atualizar o perfil do usuário logado
-router.put("/user/email", authMiddleware, updateUserEmail);   // Rota para atualizar o e-mail do usuário logado
-router.put("/role", authMiddleware, authorizeRole([UserRoles.MODERATOR, UserRoles.OWNER]), changeUserRole);   // Rota para alterar o cargo de um usuário
-
-router.delete("/user", authMiddleware, deleteOwnAccount); // Deletar própria conta
-router.delete("/users/:userId", authMiddleware, authorizeRole([UserRoles.OWNER]), deleteUser); // Owner deleta outro usuário
+router.get("/user", authMiddleware, getUserProfile);        // Rota para pegar o perfil do usuário logado
+router.get("/user/role", authMiddleware, getUserRole);      // Rota para pegar o cargo do usuário logado
+router.put("/user", authMiddleware, updateUserProfile);     // Rota para atualizar o perfil do usuário logado
+router.put("/user/email", authMiddleware, updateUserEmail); // Rota para atualizar o e-mail do usuário logado
+router.delete("/user", authMiddleware, deleteOwnAccount);   // Deletar própria conta
 
 export default router;
+
