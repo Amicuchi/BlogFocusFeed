dc3f726df39097ad482de3c7e4f46b008fc85c27
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2025-01-06 23:27:07 -0300

    style(authMiddleware): Correção de espaços

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,31 +1,31 @@
 import jwt from "jsonwebtoken";
 
 const validateToken = (token) => {
-  if (!token) {
-    throw new AuthError("Token não fornecido");
-  }
-
-  try {
-    return jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    if (error.name === "TokenExpiredError") {
-        throw new AuthError("Token expirado", 401);
+    if (!token) {
+        throw new AuthError("Token não fornecido");
     }
-    throw new AuthError("Token inválido", 401);
-  }
+
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            throw new AuthError("Token expirado", 401);
+        }
+        throw new AuthError("Token inválido", 401);
+    }
 };
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
- 
-  try {
-    const decoded = validateToken(token);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    console.error("Erro na autenticação:", error.message);
-    res.status(error.statusCode || 401).json({ message: error.message });
-  }
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    try {
+        const decoded = validateToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error("Erro na autenticação:", error.message);
+        res.status(error.statusCode || 401).json({ message: error.message });
+    }
 };
 
 export default authMiddleware;
