b4307cf822172e4983da5eb65bb7e70ec7924cb9
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2025-01-06 23:34:36 -0300

    fix(Dashoboard): Inclusão da busca do cargo para informar para a UserTable.

--- frontend/src/pages/UserAuthenticated/Dashboard/Dashboard.jsx
+++ frontend/src/pages/UserAuthenticated/Dashboard/Dashboard.jsx
@@ -1,34 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UserTable from "./components/UserTable";
 import PostsTable from "./components/PostsTable";
 import styles from "./Dashboard.module.css";
+import apiServices from "../../../services/apiServices";
+import { useAuth } from "../../../contexts/AuthContext";
 
 const Dashboard = () => {
     const [activeTab, setActiveTab] = useState("users");  // Aba ativa
+    const [role, setRole] = useState("");                 // Estado para o papel do usuário
+    const { user } = useAuth();                           // Recupera os dados do usuário e função de logout
+
+    // Obtém o papel (role) do usuário
+    useEffect(() => {
+        const fetchRole = async () => {
+            if (user?.id) {
+                try {
+                    const userRole = await apiServices.getUserRole();
+                    setRole(userRole.data.role);    // Retorna o papel do usuário e define o estado
+                } catch (error) {
+                    console.error("Erro ao buscar o papel do usuário:", error);
+                }
+            }
+        };
+
+        fetchRole();
+    }, [user?.id]);
 
     return (
         <div className={styles.dashboard}>
-          <h1 className={styles.title}>Painel de Controle</h1>
-          <div className={styles.tabs}>
-            <button
-              className={`${styles.tabButton} ${activeTab === "users" ? styles.active : ""}`}
-              onClick={() => setActiveTab("users")}
-            >
-              Usuários
-            </button>
-            <button
-              className={`${styles.tabButton} ${activeTab === "posts" ? styles.active : ""}`}
-              onClick={() => setActiveTab("posts")}
-            >
-              Posts
-            </button>
-          </div>
-          <div className={styles.content}>
-            {activeTab === "users" && <UserTable />}
-            {activeTab === "posts" && <PostsTable />}
-          </div>
+            <h1 className={styles.title}>Painel de Controle</h1>
+            <div className={styles.tabs}>
+                <button
+                    className={`${styles.tabButton} ${activeTab === "users" ? styles.active : ""}`}
+                    onClick={() => setActiveTab("users")}
+                >
+                    Usuários
+                </button>
+                <button
+                    className={`${styles.tabButton} ${activeTab === "posts" ? styles.active : ""}`}
+                    onClick={() => setActiveTab("posts")}
+                >
+                    Posts
+                </button>
+            </div>
+            <div className={styles.content}>
+                {activeTab === "users" && <UserTable loggedInUserRole={role} />}
+                {activeTab === "posts" && <PostsTable loggedInUserRole={role} />}
+            </div>
         </div>
-      );
-    };
+    );
+};
 
 export default Dashboard;
